8caaaef491e8caac21374bc7bd72c5e349697cc7
m00nr4bb1t <junmo.moon8@gmail.com>
2019-10-07 21:23:09 -0400

    Implements Camera Zoom

--- src/Viewport.hx
+++ src/Viewport.hx
@@ -3,26 +3,31 @@ using Utilities.MathExtensions;
 
 class Viewport extends Object {
     public var container: Object;
+    public var zoom(default, set) = 1.;
 
-    public function new(parent) {
+    function set_zoom(x: Float): Float {
+        return scaleX = scaleY = zoom = x;
+    }
+
+    public function new(parent: Object) {
         super(parent);
         container = new Object(this);
     }
 
     public function center(entity: Entity) {
-        if (RPGHeap.map.pixelWidth > RPGHeap.WIDTH) {
-            x = Math.clamp(RPGHeap.WIDTH / 2 - RPGHeap.player.pixelX - 16, RPGHeap.WIDTH - RPGHeap.map.pixelWidth, 0);
-        } else if (RPGHeap.map.pixelWidth == RPGHeap.WIDTH) {
+        if (RPGHeap.map.pixelWidth * zoom > RPGHeap.WIDTH) {
+            x = Math.clamp(-((entity.pixelX + 16) * zoom - RPGHeap.WIDTH / 2), RPGHeap.WIDTH - RPGHeap.map.pixelWidth * zoom, 0);
+        } else if (RPGHeap.map.pixelWidth * zoom == RPGHeap.WIDTH) {
             x = 0;
         } else {
-            x = (RPGHeap.WIDTH - RPGHeap.map.pixelWidth) / 2;
+            x = (RPGHeap.WIDTH - RPGHeap.map.pixelWidth * zoom) / 2;
         }
-        if (RPGHeap.map.pixelHeight > RPGHeap.HEIGHT) {
-            y = Math.clamp(RPGHeap.HEIGHT / 2 - RPGHeap.player.pixelY - 16, RPGHeap.HEIGHT - RPGHeap.map.pixelHeight, 0);
-        } else if (RPGHeap.map.pixelHeight == RPGHeap.HEIGHT) {
+        if (RPGHeap.map.pixelHeight * zoom > RPGHeap.HEIGHT) {
+            y = Math.clamp(-((entity.pixelY + 16) * zoom - RPGHeap.HEIGHT / 2), RPGHeap.HEIGHT - RPGHeap.map.pixelHeight * zoom, 0);
+        } else if (RPGHeap.map.pixelHeight * zoom == RPGHeap.HEIGHT) {
             y = 0;
         } else {
-            y = (RPGHeap.HEIGHT - RPGHeap.map.pixelHeight) / 2;
+            y = (RPGHeap.HEIGHT - RPGHeap.map.pixelHeight * zoom) / 2;
         }
     }
 }
